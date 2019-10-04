67817fdbd3c281ec79cd40a219d16f3a47b2ea7e
anoureddine <noureddine.amrane@etu.u-pec.fr>
2019-10-04 18:41:46 +0200

    Install : morgan middleware || Init : error routes

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
+/* import lib */
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
+/* import routes */
 const usersRoutes = require("./routes/user");
 
-app.use('/user',usersRoutes);
+/* use morgan*/
+app.use(morgan("dev"));
+
+/* filter routes*/
+app.use('/user',usersRoutes); // url : /user
+
+/* catch unfound routes */
+app.use((req, res, next) => {
+    const  error = new Error("Not found");
+    error.status(400);
+    next(error);
+});
+
+/* respons errors */
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 module.exports = app;
