2eb43e5da2d29d8a43e8a67d13cdfedca1c07114
NLitissia <litissia.negaa@gmail.com>
2019-10-05 15:54:02 +0200

    commentRoutes

---
+++ Back-End/routes/comment.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET method */
+router.get("/", (req, res, next) => {
+   res.status(200).json({
+       message : "hi"
+   })
+});
+
+/* POST method */
+router.post("/", (req, res, next) => {
+    const comment = {
+      id_comment : req.body.id_comment,
+      id_image : req.body.id_image,
+      id_user : req.body.id_user,
+      comment : req.body.comment,
+      create_at : req.body.comment
+    };
+
+    res.status(201).json(comment)
+});
+
+/* DELETE method*/
+router.delete("/:id",(req, res, next)=> {
+    const comment = {
+        id_comment : req.params.id
+    };
+
+    res.status(202).json(comment)
+      
+});
+
+/*Update method */
+router.post("/:id",(req,res, next)=>{
+    const comment = {
+        id_comment : req.params.id,
+        comment : req.body.comment
+    };
+    res.status(201).json(comment)
+});
+
+module.exports = router;
