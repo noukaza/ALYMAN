3ee0b581adf06afa422af513d228b6dec3e77789
anoureddine <noureddine.amrane@etu.u-pec.fr>
2019-10-07 22:34:52 +0200

    Init : controllers for likes

--- Back-End/routes/likes.js
+++ Back-End/routes/likes.js
@@ -2,30 +2,14 @@ const express = require('express');
 const router = express.Router();
 const chekauth = require("../middleware/check_auth")
 
+const likesController = require("../controllers/likes")
+
 /* GET method */
-router.get("/:id", chekauth, (req, res, next) => {
-    const id = req.params.id
-    res.json({
-        id: id
-    })
-});
+router.get("/:id", chekauth, likesController.get_like_by_id);
 
 /* POST method */
-router.post("/", (req, res, next) => {
-    const like = {
-        _id: req.body.id_like,
-        id_image: req.body.id_image,
-        id_user: req.body.id_user,
-        create_at: req.body.create_at
+router.post("/", likesController.poste_like);
 
-    };
-    res.status(201).json(like)
-});
-router.delete("/:id", (req, res, next) => {
-    const id = req.params.id
-
-    res.status(202).json(id)
-});
-
+router.delete("/:id", likesController.delete_like);
 
 module.exports = router;
