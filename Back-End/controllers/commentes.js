879fcdb9656f624aed56ed4ae3c5c3abbc95cd47
messaoui <mmessaoui@bigapps.fr>
2019-10-09 21:19:17 +0200

    controllers/commentes : gérer toute les méthode sauf update, controllers/commentes : gérer toutes les méthodes

--- Back-End/controllers/commentes.js
+++ Back-End/controllers/commentes.js
@@ -1,25 +1,91 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const Commente = require("../models/like");
+const User = require("../models/user");
+const Image = require("../models/image");
+
 exports.get_all_comments = (req, res, next) => {
-    res.status(200).json({
-        message: "hi"
-    })
+    Commentes
+    .find()
+    .select("_id image user comment create_at update_at")
+    //.populate("images")
+    .exec()
+    .then(data => {
+        res.status(200).json(data)
+    }).catch(err => console.log(err))
 }
 
 exports.post_comment = (req, res, next) => {
-    const comment = {
-        id_comment: req.body.id_comment,
-        id_image: req.body.id_image,
-        id_user: req.body.id_user,
-        comment: req.body.comment,
-        create_at: req.body.comment
-    };
-    res.status(201).json(comment)
+    User.find({
+        email: req.body.email
+    })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                Image.find({
+                    image: req.body.image
+                })
+                    .exec()
+                    .then(img => {
+                        if (img.length >= 1) {
+                            const commente = new Commente({
+                                    _id: req.body._id,
+                                    image: req.body.image,
+                                    user: req.body.user,
+                                    comment: req.body.comment,
+                                    create_at: req.body.create_at,
+                                    update_at:req.body.update_at
+                                })
+                            commente
+                                .save()
+                                .then(data => {
+                                    res.status(201).json(data)
+                                })
+                                .catch(err => {
+                                    res.status(500).json({
+                                        error: err
+                                    })
+                                })                     
+                               } else {
+                            res.status(409).json({
+                                message: "l'image n'existe pas"
+                            })
+                        }
+                    })
+                    .catch(err => {
+                        res.status(500).json({
+                            error: err
+                        })
+                    });
+            } else {
+                res.status(409).json({
+                    message: "l'utilisateur n'existe pas"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
+   
 }
 
 exports.delete_comment = (req, res, next) => {
-    const comment = {
-        id_comment: req.params.id
-    };
-    res.status(202).json(comment)
+    Commente.remove({ _id: req.params.id })
+    .exec()
+    .then(result => {
+        res.status(200).json({
+            message: "Done !"
+        });
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
+    })
 }
 
 exports.update_comment = (req, res, next) => {
--- Back-End/controllers/likes.js
+++ Back-End/controllers/likes.js
@@ -1,21 +1,88 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const Like = require("../models/like");
+const User = require("../models/user");
+const Image = require("../models/image");
+
 exports.get_like_by_id = (req, res, next) => {
-    const id = req.params.id
-    res.json({
-        id: id
-    })
+    Like
+        .find()
+        .select("_id user create_at update_at")
+        //.populate("images")
+        .exec()
+        .then(data => {
+            res.status(200).json(data)
+        }).catch(err => console.log(err))
 }
 
 exports.poste_like = (req, res, next) => {
-    const like = {
-        _id: req.body.id_like,
-        id_image: req.body.id_image,
-        id_user: req.body.id_user,
-        create_at: req.body.create_at
-    };
-    res.status(201).json(like)
+
+    User.find({
+        email: req.body.email
+    })
+        .exec()
+        .then(user => {
+            if (user.length >= 1) {
+                Image.find({
+                    image: req.body.image
+                })
+                    .exec()
+                    .then(img => {
+                        if (img.length >= 1) {
+                            const image = new Image({
+                                    _id: req.body.id_like,
+                                    id_image: req.body.id_image,
+                                    id_user: req.body.id_user,
+                                    create_at: req.body.create_at,
+                                    update_at:req.body.update_at
+                                })
+                            image
+                                .save()
+                                .then(data => {
+                                    res.status(201).json(data)
+                                })
+                                .catch(err => {
+                                    res.status(500).json({
+                                        error: err
+                                    })
+                                })                        } else {
+                            res.status(409).json({
+                                message: "l'image n'existe pas"
+                            })
+                        }
+                    })
+                    .catch(err => {
+                        res.status(500).json({
+                            error: err
+                        })
+                    });
+            } else {
+                res.status(409).json({
+                    message: "l'utilisateur n'existe pas"
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
+    
 }
 
 exports.delete_like = (req, res, next) => {
-    const id = req.params.id
-    res.status(202).json(id)
+    Like.remove({ _id: req.params.id })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Done !"
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        })
 }
