f9d90fe709f06feac33219bfab9dd9ca46e92372
nouredine amrane <amranenoureddin3@gmail.com>
2019-10-26 15:10:20 +0200

    Clean : code

--- Back-End/configurations/mognodb.js
+++ Back-End/configurations/mognodb.js
@@ -1,15 +1,15 @@
 
 let authenticationMongo = "";
 if (process.env.MONGODB_USER !== "") {
-    authenticationMongo = process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD + "@";
+    authenticationMongo = `${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@`;
 }
 
-let mongodbUri = process.env.MONGODB_PROTOCOL + "://" + authenticationMongo + process.env.MONGODB_HOST + ":" ;
-mongodbUri +=   process.env.MONGODB_PORT !== "" ?  + process.env.MONGODB_PORT + "/" + process.env.MONGODB_DATA : "/" + process.env.MONGODB_DATA ;
+let mongodbUri = `${process.env.MONGODB_PROTOCOL}://${authenticationMongo}${process.env.MONGODB_HOST}:`;
+mongodbUri += process.env.MONGODB_PORT !== "" ? + process.env.MONGODB_PORT + "/" + process.env.MONGODB_DATA : "/" + process.env.MONGODB_DATA;
 
-module.exports =  {
-    mongoUri : mongodbUri,
-    option : {
+module.exports = {
+    mongoUri: mongodbUri,
+    option: {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }
