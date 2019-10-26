c70901f3090c06670f378d8d257034ada435b8a4
messaoui <mmessaoui@bigapps.fr>
2019-10-27 01:34:59 +0200

    Back-end/feat/add-follow-token-and-authorization

--- Back-End/routes/followers.js
+++ Back-End/routes/followers.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const followersController = require("../controllers/followers");
+const check_auth = require("../middleware/check_auth");
+
 
 /* GET method */
 router.get("/", followersController.get_all_followers);
@@ -47,8 +49,8 @@ router.get("/", followersController.get_all_followers);
  *            404:
  *               description: "follower not found"
  */
-router.post("/", followersController.create_follower);
+router.post("/", check_auth,  followersController.create_follower);
 
-router.delete("/:id", followersController.delete_follower);
+router.delete("/:id", check_auth, followersController.delete_follower);
 
 module.exports = router;
