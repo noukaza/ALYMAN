3804762bbe29ea97e3af971972dcaa9e4ed7df57
Yanis Tagherset <yanis.tagherset@etu.u-pec.fr>
2019-11-02 12:40:23 +0100

    correction du require Comment

--- Back-End/controllers/commentes.js
+++ Back-End/controllers/commentes.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const Commente = require("../models/like");
+const Commente = require("../models/comment");
 const User = require("../models/user");
 const Image = require("../models/image");
 
