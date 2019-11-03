1b8848d4312751d1e247927d29af49000c199fc2
nouredine amrane <amranenoureddin3@gmail.com>
2019-11-03 21:32:49 +0100

    TODO : list of todo in likes controller

--- Back-End/controllers/likes.js
+++ Back-End/controllers/likes.js
@@ -14,77 +14,94 @@ exports.get_like_by_id = (req, res, next) => {
         //.populate("images")
         .exec()
         .then(data => {
-            response(res, 200, true, "successful operation", data)
-        }).catch(err =>{
+            response(res, 200, true, "successful operation", data) // TODO : changer le msg "successful operation" par un msg qui décrit le resultat il sera probablement afficher en FRONT
+        }).catch(err => {
             response(res, 404, false, "error", err)
         })
 }
 
 exports.poste_like = (req, res, next) => {
     id = req.userData._id;
+
+    /**
+     * TODO : redondance pour vérifier si l'utilisateur est bien celui qui prétend etre il faut le faire via un middleware 
+     */
     User.find({
-        _id: id
-    })
+            _id: id
+        })
         .exec()
         .then(user => {
             if (user.length >= 1) {
+                /**
+                 * TODO : redondance pour vérifier si l'image existe il faut le faire via un middleware 
+                 */
                 Image.find({
-                    image: req.body.image
-                })
+                        image: req.body.image
+                    })
                     .exec()
                     .then(img => {
                         if (img.length >= 1) {
                             const like = new Like({
-                                    _id: mongoose.Types.ObjectId(),
-                                    user: id,
-                                    image: req.body.image,
-                                    create_at: req.body.create_at,
-                                    update_at:req.body.update_at
-                                })
+                                _id: mongoose.Types.ObjectId(),
+                                user: id,
+                                image: req.body.image,
+                                create_at: req.body.create_at,
+                                update_at: req.body.update_at
+                            })
                             like
                                 .save()
                                 .then(data => {
-                                    response(res, 201, true, "successful operation", data)
+                                    response(res, 201, true, "successful operation", data) // TODO : changer le msg "successful operation"
                                 })
                                 .catch(err => {
                                     response(res, 500, false, "error", err)
-                                })                        }
-                                 else {
-                                    response(res, 409, false, "l'image n'existe pas", err)
+                                })
+                        } else {
+                            response(res, 409, false, "l'image n'existe pas", err) // TODO : changer le msg "l'image n'existe pas" => par une phrase du genre vous avez essayé de liker une images qui n'existe pas ... etc 
                         }
                     })
                     .catch(err => {
-                        response(res, 500, false, "error", err)
+                        response(res, 500, false, "error", err) // TODO : changer le message 
                     });
             } else {
-                response(res, 409, false, "l'utilisateur n'existe pas", err)
+                response(res, 409, false, "l'utilisateur n'existe pas", err) // TODO : changer le message 
             }
         })
         .catch(err => {
-            response(res, 500, false, "error", err)
+            response(res, 500, false, "error", err) // TODO : changer le message 
         });
-    
+
 }
 
 exports.delete_like = (req, res, next) => {
     id = req.userData._id;
+     /**
+     * TODO : redondance => avoir des middleware 
+     */
     Like
-    .findById({ _id: req.params.id })
-    .exec()
-    .then(data => {
-        if (data.user == id)  {
-            Like.remove({ _id: req.params.id })
-            .exec()
-            .then(result => {
-                response(res, 200, true, "Like supprimer ")
-    })
-    .catch(err => {
-        response(res, 500, false, "error", err)
-    })
-        } else {
-            response(res, 409, false, "Like n'appartien pas a l'utilisateur", err)
-        }
-    }).catch(err =>{
-        response(res, 500, false, "error", err)
-    });
+        .findById({
+            _id: req.params.id
+        })
+        .exec()
+        .then(data => {
+            if (data.user == id) {
+                Like.remove({
+                        _id: req.params.id
+                    })
+                    .exec()
+                    .then(result => {
+                        response(res, 200, true, "Like supprimer ") // TODO : changer le message 
+                    })
+                    .catch(err => {
+                        response(res, 500, false, "error", err) // TODO : changer le message 
+                        // TODO : pas besoin d envoyer les err en paramètres elles peuvent contenire des infos sensible. si besoin c a nous de sérialiser les donnees a envoyer  
+                    })
+            } else {
+                response(res, 409, false, "Like n'appartien pas a l'utilisateur", err) // TODO : changer le message 
+                // TODO : pas besoin d envoyer les err; si besoin c a nous de sérialiser les donnees a envoyer
+            }
+        }).catch(err => {
+            response(res, 500, false, "error", err) // TODO : changer le message 
+             // TODO : pas besoin d envoyer les err .si besoin c a nous de sérialiser les donnees a envoyer 
+        });
 }
