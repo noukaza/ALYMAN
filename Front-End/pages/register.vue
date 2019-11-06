d8261ac197e9260f98cf8d53122499508e717be6
MESSAOUI <mohand-ameziane.messaoui@etu.u-pec.fr>
2019-11-06 17:29:39 +0100

    pages->register créer le formulaire d'inscription

--- Front-End/pages/register.vue
+++ Front-End/pages/register.vue
@@ -1,86 +1,108 @@
 <template>
-   <b-container>
-     <div class="row">
-       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-             <img style="width: 250px" src="~/assets/logo2.png">
+<div class="container">
+
+  <div class="Col">
+      <div class="row justify-content-md-center">
+       <div class="Col Center">
+             <b-img src="~/assets/logo2.png" style="width :50%; height :auto"></b-img>
        </div>
-     </div>
- <form class="flex flex-col" @submit="submitRegister">
-   <div class="row">
-     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-   <label>First Name</label>
-   <input
-     id="nom"
-     v-model="nom"
-     type="text"
-     name="nom"
-     placeholder="Nom"
-   >
-     </div>
-   </div>
+      </div>
+      <b-form-group id="input-group-2" label="Your firstName:" label-for="input-2" class="Row">
+        <b-form-input
+          id="input-2"
+          v-model="form.firstName"
+          required
+          placeholder="Enter firstName"
+        ></b-form-input>
+      </b-form-group>
 
+      <b-form-group id="input-group-2" label="Your lastName:" label-for="input-2" class="Row">
+        <b-form-input
+          id="input-2"
+          v-model="form.lastName"
+          required
+          placeholder="Enter lastName"
+        ></b-form-input>
+      </b-form-group>
 
-   <div class="row">
-     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-   <label>Last Name</label>
-   <input
-     id="prenom"
-     v-model="prenom"
-     type="text"
-     name="prenom"
-     placeholder="Prénom"
-   >
-     </div>
-   </div>
+      <b-form-group id="input-group-2" label="Your bio:" label-for="input-2" class="Row">
+        <b-form-input
+          id="input-2"
+          v-model="form.bio"
+          required
+          placeholder="Enter bio"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="input-group-2" label="Your email:" label-for="input-2" class="Row">
+       <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
 
-   <div class="row">
-     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-   <label>E-mail adress</label>
-   <input
-     id="email"
-     v-model="email"
-     type="text"
-     name="email"
-     placeholder="xxx@exemple.com"
-   >
-     </div>
-   </div>
+      <b-form-group id="input-group-2" label="Your password:" label-for="input-2" class="Row">
+        <b-form-input
+          id="input-2"
+          v-model="form.password"
+          type="password"
+          required
+          placeholder="Enter password"
+        ></b-form-input>
+      </b-form-group>
 
-   <div class="row">
-     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-   <label>Password</label>
-   <input
-     id="password"
-     v-model="password"
-     type="password"
-     name="password"
-   >
-     </div>
-   </div>
-   <div class="row">
-     <div class="col-sm-9 col-md-4 col-lg-5 mx-auto">
-      <button type="submit" class="-border rounded py-4"> Envoyer</button>
-       <button type="submit" class="-border rounded py-4"> Annuler</button>
-     </div>
-   </div>
- </form>
-   </b-container>
+      <b-button type="submit" variant="primary"  v-on:click="onCreate">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
+  </div>
 </template>
 
 <script>
-  export default {
-      data(){
-          return {
-              nom: '',
-              prenom: '',
-              email:'',
-          }
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 
-      },
-      methods:{
-          submitRegister(){
-             console.log("cliquer sur le boutton")
-          }
+Vue.use(BootstrapVue)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+  export default {
+    data() {
+      return {
+        form: {
+          firstName:'',
+          lastName:'',
+          bio:'',
+          email: '',
+          password:'',
+        },
+        show: true
       }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      },
+      onReset(evt) {
+        evt.preventDefault()
+        this.form.firstName = '',
+        this.form.lastName = '',
+        this.form.bio = '',
+        this.form.email = '',
+        this.password = ''
+      },
+      onCreate(evt) {
+        evt.preventDefault()
+         axios.post(`https://localhost:3000/users`, {
+            body: this.form,
+          })
+            .then(response => {})
+            .catch(e => {
+                this.errors.push(e)
+          })
+    }
+  }
   }
 </script>
