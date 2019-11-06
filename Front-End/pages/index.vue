32884be0190850606ed37d523b6a87bfc3c8d491
NLitissia <litissia.negaa@gmail.com>
2019-11-06 10:38:50 +0100

    modif

--- Front-End/pages/index.vue
+++ Front-End/pages/index.vue
@@ -1,5 +1,11 @@
+
 <template>
   <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+    <img style="width: 500px" src="~/assets/logo.png">
+      </div>
+    </div>
     <div class="row">
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card card-signin my-5">
@@ -9,7 +15,6 @@
               <div class="form-label-group">
                 <input type="email"  class="form-control" v-model="email" placeholder="Email address" required autofocus>
               </div>
-            
 
               <div class="form-label-group mt-3">
                 <input type="password"  v-model="password" class="form-control" placeholder="Password" required>
@@ -27,10 +32,12 @@
 </template>
 
 <script>
-// import axios from "@nuxtjs/axios";
 
+// import axios from "@nuxtjs/axios";
+import BootstrapVue from "bootstrap-vue";
 export default {
-  
+
+
   data() {
     return {
       email:"",
@@ -51,7 +58,7 @@ export default {
         this.email = "",
         this.password =""
       })
-      
+
     }
   },
 
---
+++ Front-End/pages/register.vue
@@ -0,0 +1,86 @@
+<template>
+   <b-container>
+     <div class="row">
+       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+             <img style="width: 250px" src="~/assets/logo2.png">
+       </div>
+     </div>
+ <form class="flex flex-col" @submit="submitRegister">
+   <div class="row">
+     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+   <label>First Name</label>
+   <input
+     id="nom"
+     v-model="nom"
+     type="text"
+     name="nom"
+     placeholder="Nom"
+   >
+     </div>
+   </div>
+
+
+   <div class="row">
+     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+   <label>Last Name</label>
+   <input
+     id="prenom"
+     v-model="prenom"
+     type="text"
+     name="prenom"
+     placeholder="Prénom"
+   >
+     </div>
+   </div>
+
+   <div class="row">
+     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+   <label>E-mail adress</label>
+   <input
+     id="email"
+     v-model="email"
+     type="text"
+     name="email"
+     placeholder="xxx@exemple.com"
+   >
+     </div>
+   </div>
+
+   <div class="row">
+     <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+   <label>Password</label>
+   <input
+     id="password"
+     v-model="password"
+     type="password"
+     name="password"
+   >
+     </div>
+   </div>
+   <div class="row">
+     <div class="col-sm-9 col-md-4 col-lg-5 mx-auto">
+      <button type="submit" class="-border rounded py-4"> Envoyer</button>
+       <button type="submit" class="-border rounded py-4"> Annuler</button>
+     </div>
+   </div>
+ </form>
+   </b-container>
+</template>
+
+<script>
+  export default {
+      data(){
+          return {
+              nom: '',
+              prenom: '',
+              email:'',
+          }
+
+      },
+      methods:{
+          submitRegister(){
+             console.log("cliquer sur le boutton")
+          }
+      }
+  }
+</script>
