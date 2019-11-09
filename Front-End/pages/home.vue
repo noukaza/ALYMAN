e956a4967ac1ce3c30e038f1b9fc2a7e38b15256
NLitissia <litissia.negaa@gmail.com>
2019-11-09 13:41:11 +0100

    Components/Carousel

--- Front-End/pages/home.vue
+++ Front-End/pages/home.vue
@@ -1,67 +1,22 @@
 <template>
   <div>
-    <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background=""
-      img-width="1024"
-      img-height="480"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="~assets/Pictures/fond.jpg"
-            alt="image slot"
-            center ="true"
-          ></img>
-            <div class="carousel-caption" style="top:40%">
-                                <h1 class="display-2" style="font-family: Palatino">Rejoinez vos amis sur LYMAN</h1>
-            </div>               
-        </template>
-       </b-carousel-slide>
-    
-      <b-carousel-slide>
-        <template v-slot:img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="~assets/Pictures/fond.jpg"
-            alt="image slot"
-          >
-               <div class="carousel-caption" style="top:40%">
-                                <h1 class="display-2">Atquchem</h1>
-            </div>    
-        </template>
-       </b-carousel-slide>
-    </b-carousel>
+    <Carousel :images="image"></Carousel>
   </div>
 </template>
-
 <script>
-    export default {
-        data() {
-            return {
-                slide: 0,
-                sliding: null
-            }
-        },
-        methods: {
-            onSlideStart(slide) {
-                this.sliding = true
-            },
-            onSlideEnd(slide) {
-                this.sliding = false
-            }
-        }
+import Carousel from '~/components/Carousel.vue'
+
+export default {
+  components:{
+     Carousel
+  },
+  data:function(){
+    return {
+      image:[
+        "Pictures/fond.jpg",
+        "Pictures/fond.jpg"
+      ]
     }
+  }
+}
 </script>
