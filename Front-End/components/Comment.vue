087ac945dd7aef3f6214db474ba4b6f18fa633af
NLitissia <litissia.negaa@gmail.com>
2019-11-14 22:26:35 +0100

    delete comment

--- Front-End/components/Comment.vue
+++ Front-End/components/Comment.vue
@@ -1,22 +1,20 @@
 <template>
-  <div>
-<b-container>
+<div>
+<b-container v-show="istrue">
   <b-row>
-    <b-col col lg="2">
+    <b-col cols="1">
         <b-img  v-bind="mainProps" src="https://picsum.photos/id/1005/5760/3840" rounded="circle" alt="Circle image"></b-img>
-      </b-col>
-    <b-col cols="7" md="auto"><b>username</b></b-col>
-    <b-col cols ="2"></b-col>
-    <b-col cols="2"></b-col> 
+    </b-col>
+    <b-col ><b>username</b></b-col>
   </b-row>
   <b-row>
-    <b-col col lg="2"></b-col>
-    <b-col cols="7" md="auto">Commentedhbhdbhbdhbdhb</b-col>
-    <b-col cols ="2"><font-awesome-icon icon="trash-alt"/></b-col>
-    <b-col cols="2"><font-awesome-icon icon="pencil-alt"/></b-col> 
+    <b-col cols="1"></b-col>
+    <b-col cols="2" md="auto">Commentedhbhdbhbdhbdhbcommenter</b-col>
+    <b-col cols="1"><font-awesome-icon icon="pencil-alt"/></b-col> 
+    <b-col cols="1"><font-awesome-icon icon="trash-alt" v-on:click="istrue = !istrue"/></b-col>
   </b-row>
 </b-container>
-  </div>
+</div>
 </template>
 
 <script>
@@ -27,7 +25,11 @@
        blankColor: '#777', 
        width: 30, 
        height: 30, 
-       class: 'align-middle' },  
+       class: 'align-middle' },
+
+       istrue:true
+
+     
        
     }
 }
