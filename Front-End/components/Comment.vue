c4aaaeb095f58e11fb942846bb443e6133ebe10d
MESSAOUI <mohand-ameziane.messaoui@etu.u-pec.fr>
2019-12-17 15:01:03 +0100

    TODO

--- Front-End/components/Comment.vue
+++ Front-End/components/Comment.vue
@@ -3,13 +3,13 @@
 <b-container v-show="istrue">
   <b-row>
     <b-col cols="1">
-        <b-img  v-bind="mainProps" src="https://picsum.photos/id/1005/5760/3840" rounded="circle" alt="Circle image"></b-img>
+        <b-img  v-bind="mainProps" src="https://picsum.photos/id/1005/5760/3840" rounded="circle" alt="Circle image"></b-img> <!-- // TODO : change url img  -->
     </b-col>
     <b-col ><b>username</b></b-col>
   </b-row>
   <b-row>
     <b-col cols="1"></b-col>
-    <b-col cols="2" md="auto">Commentedhbhdbhbdhbdhbcommenter</b-col>
+   <b-col cols="2" md="auto">Commentedhbhdbhbdhbdhbcommenter</b-col> <!-- // TODO : remove Commentedhbhdbhbdhbdhbcommenter -->
     <b-col cols="1"><font-awesome-icon icon="pencil-alt"/></b-col> 
     <b-col cols="1"><font-awesome-icon icon="trash-alt" v-on:click="istrue = !istrue"/></b-col>
   </b-row>
@@ -25,13 +25,13 @@
        blankColor: '#777', 
        width: 30, 
        height: 30, 
-       class: 'align-middle' },
-
-       istrue:true
+       class: 'align-middle' }, 
+      //TODO :  we don't use date to set css pram 
+       istrue:true // TODO : isTrue !! for what  !! 
 
      
        
     }
-}
-    }
+}}
+    
 </script>
