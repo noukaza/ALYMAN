f9a8ce6f7b7e841b7ba550485fd2706db522e179
Litissia Negaa <litissia.negaa@etu.u-pec.fr>
2019-12-21 23:08:43 +0100

    Comment : changer url images + passages de parametres props

--- Front-End/components/Comment.vue
+++ Front-End/components/Comment.vue
@@ -1,16 +1,16 @@
 <template>
 <div>
-<b-container v-show="istrue">
+<b-container v-show="istrue" class="clearfix">
   <b-row>
     <b-col cols="1">
-        <b-img  v-bind="mainProps" src="https://picsum.photos/id/1005/5760/3840" rounded="circle" alt="Circle image"></b-img> <!-- // TODO : change url img  -->
+        <b-img  v-bind="mainProps" v-bind:src="images" rounded="circle" alt="Circle image"></b-img>
     </b-col>
-    <b-col ><b>username</b></b-col>
+    <b-col ><b>{{this.username}}</b></b-col>
   </b-row>
   <b-row>
     <b-col cols="1"></b-col>
-   <b-col cols="2" md="auto">Commentedhbhdbhbdhbdhbcommenter</b-col> <!-- // TODO : remove Commentedhbhdbhbdhbdhbcommenter -->
-    <b-col cols="1"><font-awesome-icon icon="pencil-alt"/></b-col> 
+   <b-col cols="2" md="auto">{{ this.descriptions }}</b-col>
+    <b-col cols="1"><font-awesome-icon icon="pencil-alt"/></b-col>
     <b-col cols="1"><font-awesome-icon icon="trash-alt" v-on:click="istrue = !istrue"/></b-col>
   </b-row>
 </b-container>
@@ -18,20 +18,26 @@
 </template>
 
 <script>
-    export default {
-        data() {
-    return {
-       mainProps: { blank: false, 
-       blankColor: '#777', 
-       width: 30, 
-       height: 30, 
-       class: 'align-middle' }, 
-      //TODO :  we don't use date to set css pram 
-       istrue:true // TODO : isTrue !! for what  !! 
 
-     
-       
+    export default {
+
+      props:['images','username','descriptions'],
+
+        data() {
+
+    return {
+
+       mainProps: { blank: false,
+       blankColor: '#777',
+       width: 30,
+       height: 30,
+       class: 'align-middle' },
+      //TODO :  we don't use date to set css pram
+       istrue:true
+
+
+
     }
 }}
-    
+
 </script>
