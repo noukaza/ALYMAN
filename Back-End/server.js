bff504ef78540e8a3bdfbbe4af46bdbb08ac336f
nouredine amrane <amranenoureddin3@gmail.com>
2019-12-22 14:33:10 +0100

    FUN : change log

--- Back-End/server.js
+++ Back-End/server.js
@@ -7,5 +7,13 @@ const port = process.env.PORT || 8080;
 const server = http.createServer(app);
 
 server.listen(port, _ => {
-    console.log('Server (http://localhost:' + port + ') started')
+    console.log(`
+                |  | ||| | | |||| |   | | || ||    ##### 
+                |  | ||| | | |||| |   | | || ||  _/_ ####
+                |  | ||| | | |||| |   | | || || [.[.]-=## 
+                |  | ||| | | |||| |   | | || ||  /_    )# 
+                |  | ||| | | |||| |   | | || ||  |__/  #
+                0  1  8  4 3   9  0   8 3  2  2   \___/
+    BACK END APP ( LYMAN API )
+    Server (http://localhost:${port}) started`)
 });
