2eeb54b117c06440d3971f98689d39f855409a97
nouredine amrane <amranenoureddin3@gmail.com>
2019-12-25 15:22:22 +0100

    fix : invalid password

--- Back-End/middleware/userFields.js
+++ Back-End/middleware/userFields.js
@@ -3,9 +3,10 @@ const response = require("../configurations/responsesTempalte");
 
 
 exports.ValidPassword = (req, res, next) => {
-    console.log("hi")
     const password = req.body.password;
-    if (password.match(passwordRegx)){
+    console.log(password)
+
+    if (password.length < 16 && password.length > 6){ //TODO find regx for password 
         next()
     }else{
         response(res,500,false,"error invalid password") ;//TODO : chagne message
