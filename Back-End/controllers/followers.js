54e2e246c511f134e9d5c8b301db6a8d00f1ea00
nouredine amrane <amranenoureddin3@gmail.com>
2020-01-05 21:50:21 +0100

    Fix :  followers

--- Back-End/controllers/followers.js
+++ Back-End/controllers/followers.js
@@ -1,39 +1,39 @@
-
 const Follower = require("../models/follower");
 const User = require("../models/user");
 const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
 const response = require("../configurations/responsesTempalte");
 
 exports.get_all_followers = async (req, res, next) => {
     let followers = await Follower
-    .find()
-    .select("_id follower following create_at")
-    .populate("follower following","_id firstName lastName")
-    .exec()
-    .catch(err =>{
-        response(res, 404, false, "error", err)
-    })
+        .find()
+        .select("_id follower following create_at")
+        .populate("follower following", "_id firstName lastName")
+        .exec()
+        .catch(err => {
+            response(res, 404, false, "error", err)
+        })
     // TODO change message 
-    followers.length === 0
-    ? response(res, 404, false, " 404 ") 
-    : response(res, 200, true, "successful operation", followers)
+    followers.length === 0 ?
+        response(res, 404, false, " 404 ") :
+        response(res, 200, true, "successful operation", followers)
 }
 
 exports.create_follower = async (req, res, next) => {
     const id = req.userData._id;
-    let user = await  User
-    .findOne({_id: id})
-    .select(" _id firstName lastName profileImage")
-    .exec()
-    .catch(err => {
-        response(res, 500, false, "error", err)
-    });
+    let user = await User
+        .findOne({
+            _id: id
+        })
+        .select(" _id firstName lastName profileImage")
+        .exec()
+        .catch(err => {
+            response(res, 500, false, "error", err)
+        });
     if (user) {
         let following = await User.findOne({
             _id: req.body.following
-        }).select(" _id firstName lastName profileImage").exec().catch(err=> response(res, 409, false, "following n'existe pas", err))
-        if(following){
+        }).select(" _id firstName lastName profileImage").exec().catch(err => response(res, 409, false, "following n'existe pas", err))
+        if (following) {
             const follower = new Follower({
                 _id: mongoose.Types.ObjectId(),
                 follower: user,
@@ -47,18 +47,23 @@ exports.create_follower = async (req, res, next) => {
     }
 }
 
-exports.delete_follower =  (req, res, next) => {
+exports.delete_follower = (req, res, next) => {
     id = req.userData._id;
+
     Follower
-    .remove({ follower: req.params.id ,following :id})
-    .exec()
-    .then(data => {
-        data.n !== 0 
-        ?    response(res, 200, true, "Follower supprimer ")
-        :   response(res, 400, true, "follower n'existe pas   ")
-    })
-    .catch(err => {
-        response(res, 501, false, "error", err)
-    })
-  
+
+        .remove({
+            following: req.params.id,
+            follower: id
+        })
+        .exec()
+        .then(data => {
+            data.n !== 0 ?
+                response(res, 200, true, "Follower supprimer ") :
+                response(res, 400, true, "follower n'existe pas   ")
+        })
+        .catch(err => {
+            response(res, 501, false, "error", err)
+        })
+
 }
