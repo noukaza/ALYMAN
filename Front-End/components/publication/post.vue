36326a8a97ade1c7ce6f9bc36366367f8dc7b7be
nouredine amrane <amranenoureddin3@gmail.com>
2020-01-09 11:19:27 +0100

    Evo : add edit btn

--- Front-End/components/publication/post.vue
+++ Front-End/components/publication/post.vue
@@ -1,56 +1,65 @@
 <template>
-<div>
-  <b-card class="mb-6 mb-4 " v-show="show">
-    <b-row class="">
-      <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="12">
-        <vs-card actionable class="cardx border border-dark">
-          <div slot="header">
-            <b-link style="color:black;" :to="'/profile/'+ image.user._id">
-              <h3>
-                <vs-avatar size="50px" v-bind:src="this.$axios.defaults.baseURL + image.user.profileImage" />
-                {{image.user.firstName}} {{image.user.lastName}}
-              </h3>
-            </b-link>
-          </div>
-          <div slot="media" style=" position: relative; height: 400px; width: 100%;">
-            <img style=" position: absolute; right: 0; bottom: 0 " :src="this.$axios.defaults.baseURL + image.image ">
-          </div>
-          <div >
-            <h4>
-              <i class="material-icons">
-                favorite
-              </i>
-              {{image.likes}}
-            </h4>
-            <span> {{image.description}} </span>
-          </div>
-          <div slot="footer">
-            <vs-row vs-justify="flex-end">
-              <vs-button class="m-1" color="primary" type="gradient" v-on:click='like' icon="thumb_up"></vs-button>
-              <vs-button class="m-1" color="danger" type="gradient" v-on:click='dislike' icon="thumb_down"></vs-button>
-              <!-- <vs-button color="#3dd495" gradient-color-secondary="rgb(130, 207, 23)"  icon="edit" type="gradient"  class="m-1" v-if="image.user._id === $auth.user._id" v-on:click='editImage'></vs-button> -->
+  <div>
+    <b-card class="mb-6 mb-4 " v-show="show">
+      <b-row class="">
+        <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="12">
+          <vs-card actionable class="cardx border border-dark">
+            <div slot="header">
+              <b-link style="color:black;" :to="'/profile/'+ image.user._id">
+                <h3>
+                  <vs-avatar size="50px" v-bind:src="this.$axios.defaults.baseURL + image.user.profileImage" />
+                  {{image.user.firstName}} {{image.user.lastName}}
+                </h3>
+              </b-link>
+            </div>
+            <div slot="media" style=" position: relative; height: 400px; width: 100%;">
+              <img style=" position: absolute; right: 0; bottom: 0 " :src="this.$axios.defaults.baseURL + image.image ">
+            </div>
+            <div>
+              <h4>
+                <i class="material-icons">
+                  favorite
+                </i>
+                {{image.likes}}
+              </h4>
+              <span> {{image.description}} </span>
+            </div>
+            <div slot="footer">
+              <vs-row vs-justify="flex-end">
+                <vs-button class="m-1" color="primary" type="gradient" v-on:click='like' icon="thumb_up"></vs-button>
+                <vs-button class="m-1" color="danger" type="gradient" v-on:click='dislike' icon="thumb_down">
+                </vs-button>
+                <!-- <vs-button color="#3dd495" gradient-color-secondary="rgb(130, 207, 23)"  icon="edit" type="gradient"  class="m-1" v-if="image.user._id === $auth.user._id" v-on:click='editImage'></vs-button> -->
+                <vs-button @click="popupActivo2=true"  color="#3dd495" gradient-color-secondary="rgb(130, 207, 23)" class="m-1"  v-if="image.user._id === $auth.user._id" icon="edit" type="filled"></vs-button>
+                <vs-popup classContent="popup-example" title="Edit description" :active.sync="popupActivo2">
+                  <vs-input class="inputx w-100"  size="large"  placeholder="Placeholder" v-model="value1" />
 
-              <vs-button color="dark" color-text="rgb(50,50,50)" icon="delete_forever" type="gradient"  class="m-1" v-if="image.user._id === $auth.user._id" v-on:click='deleteImage'></vs-button>
-            </vs-row>
-          </div>
-        </vs-card>
-      </vs-col>
-    </b-row>
-    <b-card-text>
-    </b-card-text>
-    <vs-list>
-      <vs-list-header title="Comments" color="danger"></vs-list-header>
-      <li v-for="(comment,index) in comments" v-bind:key="(comment,index)"
-        style="list-style: none; padding-bottom: 10px; ">
-        <Comment :comment="comment" :userID="comment.user._id">
-        </Comment>
-      </li>
+                <vs-button color="primary"  class="mt-3" type="filled" v-on:click='editImage'>save</vs-button>
 
-    </vs-list>
-    <CommentSaisie :imageId="image._id" :image="image" :comments="comments"></CommentSaisie>
-  </b-card>
-<vs-divider color="dark" icon="watch_later"></vs-divider>
-</div>
+
+                </vs-popup>
+                <vs-button color="dark" color-text="rgb(50,50,50)" icon="delete_forever" type="gradient" class="m-1"
+                  v-if="image.user._id === $auth.user._id" v-on:click='deleteImage'></vs-button>
+              </vs-row>
+            </div>
+          </vs-card>
+        </vs-col>
+      </b-row>
+      <b-card-text>
+      </b-card-text>
+      <vs-list>
+        <vs-list-header title="Comments" color="danger"></vs-list-header>
+        <li v-for="(comment,index) in comments" v-bind:key="(comment,index)"
+          style="list-style: none; padding-bottom: 10px; ">
+          <Comment :comment="comment" :userID="comment.user._id">
+          </Comment>
+        </li>
+
+      </vs-list>
+      <CommentSaisie :imageId="image._id" :image="image" :comments="comments"></CommentSaisie>
+    </b-card>
+    <vs-divider color="dark" icon="watch_later"></vs-divider>
+  </div>
 </template>
 <script>
   import Comment from '~/components/publication/Comment';
@@ -69,15 +78,35 @@
     data() {
       return {
         comments: [],
-        show:true
+        show: true,
+        popupActivo2: false,
+        value1: this.image.description,
       }
     },
     methods: {
-      editImage(){
-        
+      editImage() {
+         this.$axios.put('/images/'+ this.image._id,{
+           description: this.value1
+         }).then(_=>{
+           this.image.description = this.value1
+           this.popupActivo2 = false
+           this.$vs.notify({
+            title: ':D',
+            text: 'success',
+            color: 'success',
+            icon: 'favorite'
+          })
+         }).catch(err=>{
+             this.$vs.notify({
+            title: ':/',
+            text: e.response.data.message,
+            color: 'danger',
+            icon: 'favorite'
+          })
+         })
       },
-      deleteImage(){
-        this.$axios.delete("/images/"+this.image._id).then(data => {
+      deleteImage() {
+        this.$axios.delete("/images/" + this.image._id).then(data => {
           this.show = false;
           this.$vs.notify({
             title: ':D',
