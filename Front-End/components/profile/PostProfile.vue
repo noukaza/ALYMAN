d107a78ec8730fa12e757fc7a364fa4f1e978f50
nouredine amrane <amranenoureddin3@gmail.com>
2020-01-09 11:21:49 +0100

    Evo : add edit btn to profile

--- Front-End/components/profile/PostProfile.vue
+++ Front-End/components/profile/PostProfile.vue
@@ -10,7 +10,15 @@
       <vs-row vs-justify="flex-end">
         <vs-button class="m-1" color="primary" type="gradient" v-on:click='like' icon="thumb_up"></vs-button>
         <vs-button class="m-1" color="danger" type="gradient" v-on:click='dislike' icon="thumb_down"></vs-button>
-        <!-- <vs-button color="#3dd495" gradient-color-secondary="rgb(130, 207, 23)"  icon="edit" type="gradient"  class="m-1" v-if="image.user === $auth.user._id" v-on:click='editImage'></vs-button> -->
+       <vs-button @click="popupActivo2=true"  color="#3dd495" gradient-color-secondary="rgb(130, 207, 23)" class="m-1"  v-if="image.user === $auth.user._id" icon="edit" type="filled"></vs-button>
+                <vs-popup classContent="popup-example" title="Edit description" :active.sync="popupActivo2">
+                  <vs-input class="inputx w-100"  size="large"  placeholder="Placeholder" v-model="value1" />
+
+                <vs-button color="primary"  class="mt-3" type="filled" v-on:click='editImage'>save</vs-button>
+
+
+                </vs-popup>
+                
         <vs-button color="dark" color-text="rgb(50,50,50)" icon="delete_forever" type="gradient"  class="m-1" v-if="image.user === $auth.user._id" v-on:click='deleteImage'></vs-button>
   
       </vs-row>
@@ -25,7 +33,9 @@
           width: 300,
           height: 300,
           class: 'm1'
-        }
+        },
+        popupActivo2: false,
+        value1: this.image.description,
       }
     },
     props: ["url", "alt", "postId","image"],
@@ -53,6 +63,27 @@
             icon: 'favorite'
           })
         })
+      },
+       editImage() {
+         this.$axios.put('/images/'+ this.image._id,{
+           description: this.value1
+         }).then(_=>{
+           this.image.description = this.value1
+           this.popupActivo2 = false
+           this.$vs.notify({
+            title: ':D',
+            text: 'success',
+            color: 'success',
+            icon: 'favorite'
+          })
+         }).catch(err=>{
+             this.$vs.notify({
+            title: ':/',
+            text: e.response.data.message,
+            color: 'danger',
+            icon: 'favorite'
+          })
+         })
       },
       like() {
         this.$axios.post("/likes", {
